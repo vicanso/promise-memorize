cc564678c726b03392a4610de5b97e810b5e22db
vicanso <vicansocanbico@gmail.com>
2016-04-14 22:06:26 +0800

    add ttl and custom hasher

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
 'use strict';
+const util = require('util');
+
 function identity(value) {
   return value;
 }
 
-function memorize(fn, hasher) {
-  const map = new Map();
-  const _hasher = hasher || identity;
-  return function() {
-    const args = Array.from(arguments);
-    const key = _hasher.apply(null, args);
-    const prevPromise = map.get(key);
-    if (prevPromise) {
-      return prevPromise;
+function get(is, arr, v) {
+  let result;
+  arr.forEach(tmp => {
+    if (is(tmp)) {
+      result = tmp;
     }
-    const p = fn();
-    map.set(key, p);
-    p.then(data => {
-      map.delete(key);
-      return data;
-    }, err => {
-      map.delete(key);
-      throw err;
-    });
-    return p;
-  };
+  });
+  return result || v;
 }
 
+function memorize(fn, _hasher, _ttl) {
+  const map = new Map();
+  const ttl = get(util.isNumber, [_hasher, _ttl], 0);
+  const hasher = get(util.isFunction, [_hasher, _ttl], identity);
+  var memorizeFn = function memorizeFn() {
+    const args = Array.from(arguments);
+    const key = hasher.apply(null, args);
+    const item = map.get(key);
+    const now = Date.now();
+    // !ttl表示只是并发的请求使用相同的promise
+    if (item && (!ttl || (now - item.createdAt < ttl))) {
+      return item.promise;
+    }
+    const p = fn();
+    map.set(key, {
+      promise: p,
+      createdAt: now,
+    });
+    if (!ttl) {
+      p.then((data) => {
+        map.delete(key);
+        return data;
+      }, (err) => {
+        map.delete(key);
+        throw err;
+      });
+    }
+    return p;
+  };
 
+  return memorizeFn;
+}
 
 module.exports = memorize;
--- test/memorize.js
+++ test/memorize.js
@@ -1,6 +1,13 @@
 'use strict';
 const assert = require('assert');
 const memorize = require('..');
+
+const delay = (ms) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
 describe('promise-memorize', () => {
   it('cache promise for parallel call', (done) => {
     let count = 0;
@@ -24,6 +31,56 @@ describe('promise-memorize', () => {
       assert.equal(count, 1);
       done();
     }).catch(done);
+  });
+
+  it('cache promise with ttl', (done) => {
+    let count = 0;
+    const originalGet = () => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(count++);
+        }, 10);
+      });
+    };
+    const get = memorize(originalGet, 100);
+    get().then(count => {
+      assert.equal(count, 0);
+      return get();
+    }).then(count => {
+      assert.equal(count, 0);
+      return delay(100).then(get);
+    }).then(count => {
+      assert.equal(count, 1);
+      done();
+    }).catch(done);
+  });
+
+  it('cache promise with custon hasher', (done) => {
+    let count = 0;
+    const originalGet = (name, id) => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(count++);
+        }, 10);
+      });
+    };
+
+    const get = memorize(originalGet, (name, id) => {
+      return `${name}_${id}`;
+    });
+
+    get().then(count => {
+      assert.equal(count, 0);
+    }).catch(done);
+
+    get().then(count => {
+      assert.equal(count, 0);
+      return get();
+    }).then(count => {
+      assert.equal(count, 1);
+      done();
+    }).catch(done);
 
   });
+
 });
